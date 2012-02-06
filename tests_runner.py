87d099584c4adc7b348b7508013f693529400270
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2012-02-06 11:28:41 +0100

    creating function which list all tests files

--- tests_runner.py
+++ tests_runner.py
@@ -2,17 +2,20 @@ import os
 
 TESTSUITE_PACKAGE_NAME = "testsuite"
 
+def is_a_testfile(f):
+    #TODO: heuristic to decide which file is a test must be imporved!
+    return 'test' in f and f.endswith('py')
+
 def list_tests_from_directory(root, test_files=[]):
     """ List all tests *.py file. *.pyc files are NOT listed """
     files = os.listdir(root)
 
     for f in files:
         if os.path.isdir(os.path.join(root,f)):
-            #I'm passing a reference of my old list, so the new files will be just appended to the list
+            #I'm passing a reference of my list, so the new files will be just appended to the list
             list_tests_from_directory(os.path.join(root,f), test_files)
         else:
-            #TODO: heuristic to decide which file is a test must be imporved!
-            if 'test' in f and f.endswith('py'):                
+            if is_a_testfile(f):    
                 test_files.append(os.path.join(root,f))
     
     return test_files
