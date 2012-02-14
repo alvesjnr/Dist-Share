6d3f74c7fb8d4a9cc4e68c58e3e784b0fafdcd3e
Antonio Ribeiro Alves <alvesjunior.antonio@gmail.com>
2012-02-14 12:05:21 +0100

    creating base of tree

--- tree.py
+++ tree.py
@@ -12,16 +12,7 @@ class CheckboxTree(object):
         
         if items:
             self.add_items(items)
-
-        # self.cl.hlist.add("CL1", text="checklist1")
-        # self.cl.hlist.add("CL1.Item1", text="subitem1")
-        # self.cl.hlist.add("CL2", text="checklist2")
-        # self.cl.hlist.add("CL2.Item1", text="subitem1")
-        # self.cl.setstatus("CL2", "on")
-        # self.cl.setstatus("CL2.Item1", "on")
-        # self.cl.setstatus("CL1", "off")
-        # self.cl.setstatus("CL1.Item1", "off")
-        # self.cl.autosetmode()
+        self.cl.autosetmode()
     
     def add_items(self, items, parent=''):
 
