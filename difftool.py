bf4f737fd3980bcae9d5aec04eab2ed9cf88b273
Antonio Ribeiro Alves <alvesjunior.antonio@gmail.com>
2012-02-21 11:28:45 +0100

    fixing small bugs on difftool

--- difftool.py
+++ difftool.py
@@ -2,18 +2,30 @@
 
 import os
 
-def get_files_tree(root, level=0):
+def get_files_tree(root, level=0, stack_on_level={}, show_hidden=False):
     
-    files = os.listdir(root)
+    if level==0:
+        print root
 
+    files = os.listdir(root)
+    stack_on_level[level] = True
     for f in files:
+        if not show_hidden and f.startswith('.'):
+            continue
         for i in range(level):
             if i != level:
-                print '│   ',
-        print '├── '+f
+                if stack_on_level[i]:
+                    print '│ ',
+                else:
+                    print '  ',
+        if f==files[-1]:
+            print '└─ '+f
+            stack_on_level[level] = False
+        else:
+            print '├─ '+f
         
         if os.path.isdir(os.path.join(root,f)):
-            get_files_tree(os.path.join(root,f),level=level+1)
+            get_files_tree(os.path.join(root,f),level=level+1, stack_on_level=stack_on_level)
 
 if __name__=='__main__':
-    get_files_tree('.')
+    get_files_tree('.', show_hidden=True)
